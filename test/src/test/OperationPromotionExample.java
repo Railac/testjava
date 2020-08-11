c5e0423b25aac45ca9e88bb64c066d813dde6e37
Railac <urur700@naver.com>
2020-08-11 15:58:39 +0900

    "testjava"

---
+++ test/src/test/OperationPromotionExample.java
@@ -0,0 +1,34 @@
+package test;
+
+public class OperationPromotionExample {
+	public static void main(String[] args) {
+//		byte byteValue1 = 10;
+//		byte byteValue2 = 20;
+////		byte byte Value3 = byteValue1 + byteValue2; //컴파일에러, 강제형변환 필요, 
+//		int intValue1 = byteValue1 + byteValue2;
+//		System.out.println(intValue1);
+//		
+//		int intValue5 = 10;
+//		double doubleValue = 10/4.0;
+//		System.out.println(doubleValue);
+//		
+//		
+//		byte i = 1;
+//		byte j = 1;
+//		int result = i + j; 
+//		
+//		
+//		float var5 = 10;
+//		System.out.println(var5);
+//		
+////		float a = 1.2; --> float 로 실수타입 선언할때는 뒤에 F붙여줘야함
+		
+		byte byteVlaue =10;
+		float floatValue = 2.5F;
+		
+		float result = 5+floatValue;
+		System.out.println(result);
+		
+		
+	}
+}
